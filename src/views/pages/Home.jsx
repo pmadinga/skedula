20b5cfb8d783962ff8313c79243060f5879eb313
Phiwa <phiwen2@gmail.com>
2022-01-11 15:59:20 +0200

    style home

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import style from './home.module.css'
 
 const Home = () => {
@@ -7,6 +7,20 @@ const Home = () => {
             <Container>
                 <h1 className={style.app_heading}>2022 App Factory</h1>
                 <hr />
+                <div className={`${style.cards} `}>
+                    <Row>
+                        <Col sm="4">
+                            <div className={`${style.card} py-2`}>
+                                <a href="#" className={style.card_link}>
+                                    <h2 className={`${style.card_heading} `}>Week 2</h2>
+                                    <p className={style.topic}>Introduction to HTML</p>
+                                    <p >a</p>
+                                </a>
+                            </div>
+                        </Col>
+                        
+                    </Row>
+                </div>
             </Container>
             
         </div>
